8cfd10e17857c6c983c584f76092a5387a9e3a57
SuraMahesh <surangamunasinghe22568@gmail.com>
2023-01-15 20:27:56 +0530

    add theme, top bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./pages/global/Topbar";
+import Dashboard from "./pages/dashboard/Index";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./pages/global/Sidebar";
+
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-     <h2>GAP</h2>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          {/* <Sidebar isSidebar={isSidebar} /> */}
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
