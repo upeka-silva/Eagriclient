c22a5bcbc6069ba86cf41de412b436330766a1c7
pasindupansilu <41730019+pasindupansilu@users.noreply.github.com>
2023-03-24 04:10:42 +0530

    password reset ui

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,13 @@
 import App from "../pages/App/App";
+import PasswordResetPage from "../pages/Password Reset";
 
 export const Routes = [
     {
         path: "/",
         element: <App />
+    },
+    {
+        path: "/password-reset",
+        element: <PasswordResetPage />
     }
 ];
