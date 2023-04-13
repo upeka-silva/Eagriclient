9bfb9e6895fd233ad86990fbc913faf079aebb58
pasindupansilu <41730019+pasindupansilu@users.noreply.github.com>
2023-04-14 03:59:48 +0530

    added: missing components to form generator

---
+++ src/utils/constants/formElementTypes.js
@@ -0,0 +1,10 @@
+export const FormElementTypes = {
+    SELECT: 'select',
+    TEXT: 'text',
+    DATE: 'date',
+    DATERANGE: 'date_range',
+    CHECKBOX: 'checkbox',
+    RADIO: 'radio',
+    SEARCHABLE: 'searchable',
+    SLIDER: 'slider',
+};
