d86d454c144ba7c03162eec79957a935f504b821
SuraMahesh <surangamunasinghe22568@gmail.com>
2023-04-25 16:13:25 +0530

    update App header

--- src/utils/constants/Colors.js
+++ src/utils/constants/Colors.js
@@ -1,6 +1,7 @@
 export const Colors = {
     white: "#FFFFFF",
     black: "#000000",
+    profileButtonBGColor: "rgba(77, 95, 124, 0.75);",
 
-    shadow: "0px 5px 10px rgba(0, 0, 0, 0.1) !important"
+    shadow: "0px 4px 4px rgba(0, 0, 0, 0.25) !important"
 };
