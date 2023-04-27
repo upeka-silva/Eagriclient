30199d04756b047b3be7253fb62a71212c83b045
SuraMahesh <surangamunasinghe22568@gmail.com>
2023-04-28 02:26:28 +0530

    add ds-division form and table

--- src/pages/Zones/DS/DsDivision.js
+++ src/pages/Zones/DS/DsDivision.js
@@ -1,8 +1,154 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PermissionWrapper from "../../../components/PermissionWrapper/PermissionWrapper";
+import { useUserAccessValidation } from "../../../hooks/authentication";
+import {
+  DEF_ACTIONS,
+  DEF_COMPONENTS,
+} from "../../../utils/constants/permission";
+import { useNavigate } from "react-router";
+import { Button } from "@mui/material";
+import { ActionWrapper } from "../../../components/PageLayout/ActionWrapper";
+import DsDivisionList from "./DsDivisionList"
 
 const DsDivision = () => {
+
+  useUserAccessValidation();
+  const navigate = useNavigate();
+
+  const [selectedDsDivisions, setSelectedDsDivisions] = useState([]);
+  const [action, setAction] = useState(DEF_ACTIONS.ADD);
+
+  const toggleDsDivisionSelect = (component) => {
+    setSelectedDsDivisions((current = []) => {
+      let newList = [...current];
+      let index = newList.findIndex((c) => c?.id === component?.id);
+      if (index > -1) {
+        newList.splice(index, 1);
+      } else {
+        newList.push(component);
+      }
+      return newList;
+    });
+  };
+
+  const selectAllDsDivisions = (all = []) => {
+    setSelectedDsDivisions(all);
+  };
+
+  const resetSelectedDsDivisions = () => {
+    setSelectedDsDivisions([]);
+  };
+
+
+  const onCreate = () => {
+    setAction(DEF_ACTIONS.ADD);
+    navigate("/zone/ds-division-form", { state: { action: DEF_ACTIONS.ADD } });
+  };
+
+  const onEdit = () => {
+    setAction(DEF_ACTIONS.EDIT);
+    navigate("/zone/ds-division-form", {
+      state: {
+        action: DEF_ACTIONS.EDIT,
+        target: setSelectedDsDivisions[0] || {},
+      },
+    });
+  };
+
+  const onView = () => {
+    setAction(DEF_ACTIONS.VIEW);
+    navigate("/zone/ds-division-form", {
+      state: {
+        action: DEF_ACTIONS.VIEW,
+        target: setSelectedDsDivisions[0] || {},
+      },
+    });
+  };
+
+
   return (
-    <div>DsDivision</div>
+    <div>
+      <ActionWrapper>
+        {/* <PermissionWrapper
+          permission={`${DEF_ACTIONS.ADD}_${DEF_COMPONENTS.DSDIVISION}`}
+        >
+          <Button variant="contained" onClick={onCreate}>
+            {DEF_ACTIONS.ADD}
+          </Button>
+        </PermissionWrapper> */}
+        <PermissionWrapper withoutPermissions>
+          <Button variant="contained" onClick={onCreate}>
+            {DEF_ACTIONS.ADD}
+          </Button>
+        </PermissionWrapper>
+        {selectedDsDivisions.length === 1 && (
+          // <PermissionWrapper
+          //   permission={`${DEF_ACTIONS.VIEW}_${DEF_COMPONENTS.DSDIVISION}`}
+          // >
+          //   <Button
+          //     variant="contained"
+          //     color="secondary"
+          //     onClick={onEdit}
+          //     sx={{ ml: "8px" }}
+          //   >
+          //     {DEF_ACTIONS.EDIT}
+          //   </Button>
+          // </PermissionWrapper>
+          <PermissionWrapper withoutPermissions>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={onEdit}
+              sx={{ ml: "8px" }}
+            >
+              {DEF_ACTIONS.EDIT}
+            </Button>
+          </PermissionWrapper>
+        )}
+         {selectedDsDivisions.length === 1 && (
+          //      <PermissionWrapper
+          //      permission={`${DEF_ACTIONS.VIEW}_${DEF_COMPONENTS.DSDIVISION}`}
+          //  >
+          //      <Button
+          //          variant='contained'
+          //          color='info'
+          //          onClick={onView}
+          //          sx={{ ml: '8px' }}
+          //      >
+          //          {DEF_ACTIONS.VIEW}
+          //      </Button>
+          //  </PermissionWrapper>
+          <PermissionWrapper withoutPermissions>
+            <Button
+              variant="contained"
+              color="info"
+              onClick={onView}
+              sx={{ ml: "8px" }}
+            >
+              {DEF_ACTIONS.VIEW}
+            </Button>
+          </PermissionWrapper>
+        )}
+      </ActionWrapper>
+       {/* <PermissionWrapper
+        permission={`${DEF_ACTIONS.VIEW_LIST}_${DEF_COMPONENTS.DSDIVISION}`}
+      >
+        <ProvinceList
+          selectedRows={selectedProvinces}
+          onRowSelect={toggleProvinceSelect}
+          selectAll={selectAllProvinces}
+          unSelectAll={resetSelectedProvinces}
+        />
+      </PermissionWrapper> */}
+      <PermissionWrapper withoutPermissions>
+        <DsDivisionList
+          selectedRows={selectedDsDivisions}
+          onRowSelect={toggleDsDivisionSelect}
+          selectAll={selectAllDsDivisions}
+          unSelectAll={resetSelectedDsDivisions}
+        />
+      </PermissionWrapper>
+    </div>
   )
 }
 
--- src/pages/Zones/DS/DsDivisionList.js
+++ src/pages/Zones/DS/DsDivisionList.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { CardWrapper } from "../../../components/PageLayout/Card";
+import { DataTable } from "../../../components/PageLayout/Table";
 
-const DsDivisionList = () => {
+const DsDivisionList = ({
+  selectedRows = [],
+  onRowSelect = (_c) => {},
+  selectAll = (_list = []) => {},
+  unSelectAll = () => {},
+}) => {
+  const columns = [
+    { field: "code", headerName: "DS Division Code" },
+    { field: "name", headerName: "DS Division Name" },
+    { field: "districtDTOList", headerName: "District Name" },
+  ];
+
   return (
-    <div>DsDivisionList</div>
-  )
-}
+    <CardWrapper>
+      <DataTable
+        loadingTable
+        dataEndPoint={"geo-data/ds-divisions"}
+        columns={columns}
+        selectable
+        selectedRows={selectedRows}
+        selectAll={selectAll}
+        onRowSelect={onRowSelect}
+        unSelectAll={unSelectAll}
+      />
+    </CardWrapper>
+  );
+};
 
-export default DsDivisionList
+export default DsDivisionList;
