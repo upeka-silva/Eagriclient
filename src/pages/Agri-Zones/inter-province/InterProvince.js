295ab6f2d46d8905067e8554f664a1a7f641f96b
SuraMahesh <surangamunasinghe22568@gmail.com>
2023-05-01 14:56:57 +0530

    update inter province area from and tabel

--- src/pages/Agri-Zones/inter-province/InterProvince.js
+++ src/pages/Agri-Zones/inter-province/InterProvince.js
@@ -1,182 +1,142 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import { ActionWrapper } from "../../../components/PageLayout/ActionWrapper";
 import PermissionWrapper from "../../../components/PermissionWrapper/PermissionWrapper";
-import { Button, Typography } from "@mui/material";
-import PlusIcon from "@mui/icons-material/Add";
-import theme from "../../../utils/theme/theme.json";
+import { Button } from "@mui/material";
 import InterProvinceList from "./InterProvinceList";
-import InterProvinceForm from "./InterProvinceForm";
-import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import { useNavigate } from "react-router-dom";
+import { useUserAccessValidation } from "../../../hooks/authentication";
+import { DEF_ACTIONS, DEF_COMPONENTS } from "../../../utils/constants/permission";
 
 const InterProvince = () => {
-  const navigation = new useNavigate()
 
-  const [selectedInterProvince, setSelectedInterProvince] = useState(null);
-  const [selectedInterProvinces, setSelectedInterProvinces] = useState([]);
-  const [action, setAction] = useState("new");
-  const [dialogState, setDialogState] = useState(false);
+  useUserAccessValidation();
+  const navigate = useNavigate();
 
-  const openDialog = () => {
-    setDialogState(true);
-  };
+  const [selectedInterProvinceArea, setSelectedInterProvinceArea] = useState([]);
+  const [action, setAction] = useState(DEF_ACTIONS.ADD);
 
-  const closeDialog = () => {
-    setDialogState(false);
-  };
-
-  const onCreate = useCallback(() => {
-    setAction("new");
-    openDialog();
-    navigation("/agri-zone/inter-province-area-form")
-  }, []);
-
-  const onView = useCallback((province) => {
-    setSelectedInterProvince(province);
-    setAction("view");
-    openDialog();
-  }, []);
-
-  const onEdit = useCallback((province) => {
-    setSelectedInterProvince(province);
-    setAction("edit");
-    openDialog();
-  }, []);
-
-  const onDelete = useCallback((province) => {
-    setSelectedInterProvince(province);
-    setAction("delete");
-    openDialog();
-  }, []);
-
-  const updateInterProvince = (value, key) => {
-    setSelectedInterProvince((current) => ({ ...current, [key]: value }));
-  };
-
-  const selectAllInterProvinces = (provinces) => {
-    setSelectedInterProvinces(provinces);
-  };
-
-  const removeAllSelectedInterProvinces = () => {
-    setSelectedInterProvinces([]);
-  };
-
-  const toggleSelectedInterProvinces = (province) => {
-    setSelectedInterProvinces((current = []) => {
-      let exists = current.findIndex((c) => c?.id === province.id) > -1;
-      if (exists) {
-        return current.filter((c) => c.id !== province.id);
+  const toggleInterProvinceAreaSelect = (component) => {
+    setSelectedInterProvinceArea((current = []) => {
+      let newList = [...current];
+      let index = newList.findIndex((c) => c?.id === component?.id);
+      if (index > -1) {
+        newList.splice(index, 1);
+      } else {
+        newList.push(component);
       }
-      return [...current, province];
+      return newList;
     });
   };
 
-  const generatePopUpBody = () => {
-    switch (action) {
-      case "new":
-      case "edit":
-        return (
-          <InterProvinceForm
-            selectedInterProvince={selectedInterProvince}
-            updateInterProvince={updateInterProvince}
-          />
-        );
-      case "view":
-        return (
-          <div>
-            <Typography>
-              <br />
-              <b>Province Cocde: </b>
-              {selectedInterProvince?.provinceCode}
-            </Typography>
-            <Typography>
-              <b>Province Name: </b>
-              {selectedInterProvince?.name}
-            </Typography>
-          </div>
-        );
-      case "delete":
-        return (
-          <Typography>
-            <br />
-            Do you want to delete this record?
-          </Typography>
-        );
-      default:
-        return null;
-    }
+  const selectAllInterProvinceArea = (all = []) => {
+    setSelectedInterProvinceArea(all);
   };
 
-  const generatePopUpTitle = () => {
-    switch (action) {
-      case "new":
-        return "New Inter Province";
-      case "view":
-        return selectedInterProvince?.name;
-      case "edit":
-        return `Editing ${selectedInterProvince?.name}`;
-      case "delete":
-        return `Deleting ${selectedInterProvince?.name}`;
-      default:
-        return null;
-    }
+  const resetSelectedInterProvinceArea = () => {
+    setSelectedInterProvinceArea([]);
+  }
+
+  const onCreate = () => {
+    setAction(DEF_ACTIONS.ADD);
+    navigate("/agri-zone/inter-province-area-form", { state: { action: DEF_ACTIONS.ADD } });
   };
 
-  const onConfirm = () => {
-    closeDialog();
+  const onEdit = () => {
+    setAction(DEF_ACTIONS.EDIT);
+    navigate("/agri-zone/inter-province-area-form", {
+      state: {
+        action: DEF_ACTIONS.EDIT,
+        target: selectedInterProvinceArea[0] || {},
+      },
+    });
   };
+
+  const onView = () => {
+    setAction(DEF_ACTIONS.VIEW);
+    navigate("/agri-zone/inter-province-area-form", {
+      state: {
+        action: DEF_ACTIONS.VIEW,
+        target: selectedInterProvinceArea[0] || {},
+      },
+    });
+  };
+
 
   return (
     <div>
       <ActionWrapper>
-        <PermissionWrapper
-          component={
+      <PermissionWrapper withoutPermissions>
+          <Button variant="contained" onClick={onCreate}>
+            {DEF_ACTIONS.ADD}
+          </Button>
+        </PermissionWrapper>
+        {selectedInterProvinceArea.length === 1 && (
+          // <PermissionWrapper
+          //   permission={`${DEF_ACTIONS.EDIT}_${DEF_COMPONENTS.INTER_PROVINCE_AREA}`}
+          // >
+          //   <Button
+          //     variant="contained"
+          //     color="secondary"
+          //     onClick={onEdit}
+          //     sx={{ ml: "8px" }}
+          //   >
+          //     {DEF_ACTIONS.EDIT}
+          //   </Button>
+          // </PermissionWrapper>
+          <PermissionWrapper withoutPermissions>
             <Button
-              variant="container"
-              startIcon={<PlusIcon />}
-              sx={{ background: theme.coreColors.secondary }}
-              onClick={onCreate}
-              
+              variant="contained"
+              color="secondary"
+              onClick={onEdit}
+              sx={{ ml: "8px" }}
             >
-              ADD
+              {DEF_ACTIONS.EDIT}
             </Button>
-          }
-        />
-        {selectedInterProvinces.length === 1 ? (
-          <PermissionWrapper
-            component={<Button variant="container" color="info" />}
-          />
-        ) : null}
-        {selectedInterProvinces.length === 1 ? (
-          <PermissionWrapper
-            component={
-              <Button
-                variant="contained"
-                color="info"
-                startIcon={<ModeEditIcon />}
-                sx={{ ml: "5px" }}
-                onClick={onCreate}
-              >
-                Update
-              </Button>
-            }
-          />
-        ) : null}
+          </PermissionWrapper>
+        )}
+       {selectedInterProvinceArea.length === 1 && (
+          // <PermissionWrapper
+          //   permission={`${DEF_ACTIONS.VIEW}_${DEF_COMPONENTS.INTER_PROVINCE_AREA}`}
+          // >
+          //   <Button
+          //     variant="contained"
+          //     color="info"
+          //     onClick={onView}
+          //     sx={{ ml: "8px" }}
+          //   >
+          //     {DEF_ACTIONS.VIEW}
+          //   </Button>
+          // </PermissionWrapper>
+          <PermissionWrapper withoutPermissions>
+            <Button
+              variant="contained"
+              color="info"
+              onClick={onView}
+              sx={{ ml: "8px" }}
+            >
+              {DEF_ACTIONS.VIEW}
+            </Button>
+          </PermissionWrapper>
+        )}
       </ActionWrapper>
-      <PermissionWrapper
-        component={
-          <InterProvinceList
-            onView={onView}
-            onEdit={onEdit}
-            onDelete={onDelete}
-            selectedInterProvinces={selectedInterProvinces}
-            setSelectedInterProvinces={toggleSelectedInterProvinces}
-            selectAllInterProvinces={selectAllInterProvinces}
-            removeSelectedInterProvinces={removeAllSelectedInterProvinces}
-          />
-        }
-      />
- 
+      {/* <PermissionWrapper
+        permission={`${DEF_ACTIONS.VIEW_LIST}_${DEF_COMPONENTS.INTER_PROVINCE_AREA}`}
+      >
+        <ProvinceList
+          selectedRows={selectedProvinces}
+          onRowSelect={toggleProvinceSelect}
+          selectAll={selectAllProvinces}
+          unSelectAll={resetSelectedProvinces}
+        />
+      </PermissionWrapper> */}
+      <PermissionWrapper withoutPermissions>
+        <InterProvinceList
+          selectedRows={selectedInterProvinceArea}
+          onRowSelect={toggleInterProvinceAreaSelect}
+          selectAll={selectAllInterProvinceArea}
+          unSelectAll={resetSelectedInterProvinceArea}
+        />
+      </PermissionWrapper>
     </div>
   );
 };
