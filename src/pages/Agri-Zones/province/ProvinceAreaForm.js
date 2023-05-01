30914db297697932d7226526188652ba40fe29c5
SuraMahesh <surangamunasinghe22568@gmail.com>
2023-05-01 14:24:27 +0530

    update province area form and table

--- src/pages/Agri-Zones/province/ProvinceAreaForm.js
+++ src/pages/Agri-Zones/province/ProvinceAreaForm.js
@@ -1,107 +1,182 @@
-import React, { useState, useEffect } from "react";
-import theme from "../../../utils/theme/theme.json";
-import styled from "styled-components";
-import Card from "@mui/material/Card";
-import { Button, TextField } from "@mui/material";
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, TextField, CircularProgress } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate, useLocation } from "react-router-dom";
 import { ActionWrapper } from "../../../components/PageLayout/ActionWrapper";
+import { useUserAccessValidation } from "../../../hooks/authentication";
+import { useSnackBars } from "../../../context/SnackBarContext";
+import {
+  DEF_ACTIONS,
+  DEF_COMPONENTS,
+} from "../../../utils/constants/permission";
+import { SnackBarTypes } from "../../../utils/constants/snackBarTypes";
+import { handleProvinceArea } from "../../../redux/actions/provinceArea/action";
+
+import { FormWrapper } from "../../../components/FormLayout/FormWrapper";
+import { PathName } from "../../../components/FormLayout/PathName";
+import { FormHeader } from "../../../components/FormLayout/FormHeader";
+import { FieldWrapper } from "../../../components/FormLayout/FieldWrapper";
+import { FieldName } from "../../../components/FormLayout/FieldName";
+import { ButtonWrapper } from "../../../components/FormLayout/ButtonWrapper";
+import { AddButton } from "../../../components/FormLayout/AddButton";
+import { ResetButton } from "../../../components/FormLayout/ResetButton";
 
 const ProvinceAreaForm = () => {
-  const navigation = useNavigate()
+  useUserAccessValidation();
+  const { state } = useLocation();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
-    name: "",
-  });
+  const [formData, setFormData] = useState(state?.target || {});
+  const [saving, setSaving] = useState(false);
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setFormData((current) => ({
-      ...current,
-      [e?.target?.name]: e?.target?.value || "",
-    }));
+  const { addSnackBar } = useSnackBars();
+
+  const goBack = () => {
+    navigate("/agri-zone/province-area");
   };
 
-  useEffect(() => {
-    console.log(formData);
-  }, [formData]);
-
-  const handleReset = (e) => {
-    e.preventDefault();
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      name: "",
-    }));
+  const handleChange = (value, target) => {
+    setFormData((current = {}) => {
+      let newData = { ...current };
+      newData[target] = value;
+      return newData;
+    });
   };
 
-  const onBack = () => {
-    navigation("/agri-zone/province-area")
-  }
+  const resetForm = () => {
+    if (state?.action === DEF_ACTIONS.EDIT) {
+      setFormData(state?.target || {});
+    } else {
+      setFormData({});
+    }
+  };
+
+  const enableSave = () => {
+    if (state?.action === DEF_ACTIONS.EDIT) {
+      if (JSON.stringify(state?.target || {}) !== JSON.stringify(formData)) {
+        return true;
+      }
+    }
+    if (
+      state?.action === DEF_ACTIONS.ADD &&
+      Object.keys(formData || {}).length > 0
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  const onSuccess = () => {
+    addSnackBar({
+      type: SnackBarTypes.success,
+      message:
+        state?.action === DEF_ACTIONS.ADD
+          ? "Successfully Added"
+          : "Successfully Updated",
+    });
+    setSaving(false);
+  };
+
+  const onError = (message) => {
+    addSnackBar({
+      type: SnackBarTypes.error,
+      message: message || "Login Failed",
+    });
+    setSaving(false);
+  };
+
+  const handleFormSubmit = async () => {
+    if (enableSave()) {
+      setSaving(true);
+      try {
+        await handleProvinceArea(formData, onSuccess, onError);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const getPathName = () => {
+    return location.pathname === "/" || !location.pathname
+      ? ""
+      : location.pathname;
+  };
 
   return (
-    <FromCard>
-      <ActionWrapper style={{justifyContent: "flex-start"}}>
-        <Button startIcon={<KeyboardBackspaceIcon />} style={{ color: `${theme.schemes.light.onBack}` }} onClick={onBack}>
-          Back to table
+    <FormWrapper>
+      <ActionWrapper isLeft>
+        <Button startIcon={<ArrowBackIcon />} onClick={goBack}>
+          Go back to list
         </Button>
       </ActionWrapper>
-      <FormCardWrapper>
-        <FormTitle>Register Province Area</FormTitle>
+      <PathName>{getPathName()}</PathName>
+      <FormHeader>
+        {saving && <CircularProgress size={20} sx={{ mr: "8px" }} />}Add a
+        Province Area
+      </FormHeader>
+      <FieldWrapper>
+        <FieldName>Province ID</FieldName>
         <TextField
-          variant="outlined"
+          name="id"
+          id="id"
+          value={formData?.id || ""}
           fullWidth
-          label="Name of the provincial area"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          size="small"
-          prop
-          sx={{ maxWidth: 500 }}
+          disabled={state?.action === DEF_ACTIONS.VIEW}
+          onChange={(e) => handleChange(e?.target?.value || "", "id")}
+          sx={{
+            width: "264px",
+            "& .MuiInputBase-root": {
+              height: "30px",
+              borderRadius: "8px",
+            },
+          }}
         />
-      </FormCardWrapper>
-      <ButtonContainer>
-        <Button type="submit">Create</Button>
-        <Button
-          style={{ color: `${theme.schemes.light.reset}` }}
-          type="reset"
-          onClick={handleReset}
-        >
-          Reset
-        </Button>
-      </ButtonContainer>
-    </FromCard>
+      </FieldWrapper>
+      <FieldWrapper>
+        <FieldName>Description</FieldName>
+        <TextField
+          name="description"
+          id="description"
+          value={formData?.description || ""}
+          fullWidth
+          disabled={state?.action === DEF_ACTIONS.VIEW}
+          onChange={(e) => handleChange(e?.target?.value || "", "description")}
+          sx={{
+            width: "264px",
+            "& .MuiInputBase-root": {
+              height: "30px",
+              borderRadius: "8px",
+            },
+          }}
+        />
+      </FieldWrapper>
+      <ButtonWrapper>
+      {state?.action !== DEF_ACTIONS.VIEW && (
+          <ActionWrapper>
+            {saving ? (
+              <AddButton variant="contained" disabled>
+                {state?.action === DEF_ACTIONS.ADD
+                  ? "ADDING..."
+                  : "UPDATING..."}
+              </AddButton>
+            ) : (
+              <>
+                <AddButton
+                  variant="contained"
+                  disabled={!enableSave()}
+                  onClick={handleFormSubmit}
+                >
+                  {state?.action === DEF_ACTIONS.ADD ? "ADD" : "UPDATE"}
+                </AddButton>
+                <ResetButton onClick={resetForm}>RESET</ResetButton>
+              </>
+            )}
+          </ActionWrapper>
+        )}
+      </ButtonWrapper>
+    </FormWrapper>
   );
 };
 
 export default ProvinceAreaForm;
-
-const FromCard = styled(Card).attrs((props) => ({}))`
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-  padding: 10px;
-  margin: 10px;
-`;
-
-const FormCardWrapper = styled.div`
-  text-align: center;
-`;
-
-const FormTitle = styled.p`
-  font-size: 25px;
-  font-weight: 600;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: right;
-`;
-
-const BackWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  color: gray;
-`;
