d0c31e5ded24d58b5d39f69d5af3e837ed037a09
Dinidu Hewage <mailtodinidu@gmail.com>
2023-07-17 06:14:34 +0530

    make some changes to form heading

--- src/components/PageLayout/ActionWrapper.js
+++ src/components/PageLayout/ActionWrapper.js
@@ -6,3 +6,7 @@ export const ActionWrapper = styled.div`
     justify-content: ${props => props?.isLeft ? 'flex-start' : props?.isCeneter ? 'center' : 'flex-end'};
     padding: 8px 0px;
 `;
+
+export const makeCapitalize = (str) => {
+    return str?.charAt(0)?.toUpperCase() + str?.slice(1).toLowerCase();
+}
