fc3057a3f3b5bf27705914c32f97337ee9f0c0d8
Dinidu Hewage <mailtodinidu@gmail.com>
2023-07-17 06:24:19 +0530

    correct indentation issue

--- src/pages/Crop/CropCategory/CropCategoryList.js
+++ src/pages/Crop/CropCategory/CropCategoryList.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { DataTable } from '../../../components/PageLayout/Table';
-import { CardWrapper } from '../../../components/PageLayout/Card';
+import React from "react";
+import { DataTable } from "../../../components/PageLayout/Table";
+import { CardWrapper } from "../../../components/PageLayout/Card";
 
 const CropCategoryList = ({
-	selectedRows = [],
-	onRowSelect = (_c) => {},
-	selectAll = (_list = []) => {},
-	unSelectAll = () => {},
+  selectedRows = [],
+  onRowSelect = (_c) => {},
+  selectAll = (_list = []) => {},
+  unSelectAll = () => {},
 }) => {
-	const columns = [
-		{ field: 'categoryId', headerName: 'Code' },
-		{ field: 'description', headerName: 'Description' },
-	];
+  const columns = [
+    { field: "categoryId", headerName: "Code" },
+    { field: "description", headerName: "Description" },
+  ];
 
-	return (
-		<CardWrapper>
-			<DataTable
-				loadingTable
-				dataEndPoint={'geo-data/crop-categories'}
-				columns={columns}
-				selectable
-				selectedRows={selectedRows}
-				selectAll={selectAll}
-				onRowSelect={onRowSelect}
-				unSelectAll={unSelectAll}
-			/>
-		</CardWrapper>
-	);
+  return (
+    <CardWrapper>
+      <DataTable
+        loadingTable
+        dataEndPoint={"geo-data/crop-categories"}
+        columns={columns}
+        selectable
+        selectedRows={selectedRows}
+        selectAll={selectAll}
+        onRowSelect={onRowSelect}
+        unSelectAll={unSelectAll}
+      />
+    </CardWrapper>
+  );
 };
 
 export default CropCategoryList;
