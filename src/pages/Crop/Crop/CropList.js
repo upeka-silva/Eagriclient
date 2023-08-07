b1837ca26440bbcae1f5031e90eb8df5ccaa0a74
Mr RAAN <hiafraan@gmail.com>
2023-08-07 21:32:37 +0530

    Register a Crop

---
+++ src/pages/Crop/Crop/CropList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { DataTable } from "../../../components/PageLayout/Table";
+import { CardWrapper } from "../../../components/PageLayout/Card";
+
+const CropList = ({
+  selectedRows = [],
+  onRowSelect = (_c) => {},
+  selectAll = (_list = []) => {},
+  unSelectAll = () => {},
+}) => {
+  const columns = [
+    { field: "cropId", headerName: "Crop ID" },
+    { field: "scientificName", headerName: "Scientific Name" },
+  ];
+
+  return (
+    <CardWrapper>
+      <DataTable
+        loadingTable
+        dataEndPoint={"geo-data/crops"}
+        columns={columns}
+        selectable
+        selectedRows={selectedRows}
+        selectAll={selectAll}
+        onRowSelect={onRowSelect}
+        unSelectAll={unSelectAll}
+      />
+    </CardWrapper>
+  );
+};
+
+export default CropList;
