256225fb1c9e8befd4ad7819de05b5e6befbdf42
Zenith Anthony <dabarerawza.19@uom.lk>
2023-08-19 14:55:44 +0530

    dropdownlist changes

--- src/pages/Users/UserForm.js
+++ src/pages/Users/UserForm.js
@@ -37,140 +37,9 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-
+import data from "../../dropdown/drodwnlist"
 const UsersForm = () => {
-    const data = {
-        "province": {
-            "displayName": "Province",
-            "apiCall": true,
-            "links": null
-        },
-        "district": {
-            "displayName": "District",
-            "apiCall": true,
-            "links": null
-        },
-        "AIRegionProvincial": {
-            "displayName": "AI Region",
-            "apiCall": true,
-            "links": [
-                "deputiyDirOfAgriProvincial",
-                "ADASegmantProvincial"
-            ]
-        },
-        "AIRegionInterProvincial": {
-            "displayName": "AI Region",
-            "apiCall": true,
-            "links": [
-                "deputiyDirOfAgriProvincial",
-                "ADASegmantInterProvincial"
-            ]
-        },
-        "GNDivision": {
-            "displayName": "GN Division",
-            "apiCall": true,
-            "links": [
-                "district",
-                "DSDivision"
-            ]
-        },
-        "DSDivision": {
-            "displayName": "DS Division",
-            "apiCall": false,
-            "links": [
-                "district"
-            ]
-        },
-        "ADASegmantProvincial": {
-            "displayName": "ADA Segmant Provincial",
-            "apiCall": false,
-            "links": [
-                "deputiyDirOfAgriProvincial"
-            ]
-        },
-        "ADASegmantInterProvincial": {
-            "displayName": "ADA Segmant Inter Provincial",
-            "apiCall": false,
-            "links": [
-                "deputiyDirOfAgriInterProvincial"
-            ]
-        },
-        "deputiyDirOfAgriProvincial": {
-            "displayName": "Deputiy Director Of Agriculture Provincial",
-            "apiCall": false,
-            "links": null
-        },
-        "deputiyDirOfAgriInterProvincial": {
-            "displayName": "Deputiy Director Of Agriculture Inter Provincial",
-            "apiCall": false,
-            "links": null
-        },
-        "mahaweliUnit": {
-            "displayName": "Mahaweli Unit",
-            "apiCall": false,
-            "links": [
-                "mahaweliSystems",
-                "block"
-            ]
-        },
-        "block": {
-            "displayName": "Block",
-            "apiCall": false,
-            "links": [
-                "mahaweliSystems"
-            ]
-        },
-        "mahaweliSystems": {
-            "displayName": "Mahaweli Systems",
-            "apiCall": false,
-            "links": null
-        },
-        "ARPADivision": {
-            "displayName": "ARPA Division",
-            "apiCall": false,
-            "links": [
-                "districtCommisioner",
-                "ASCDivision",
-            ]
-        },
-        "ASCDivision": {
-            "displayName": "ASC Division",
-            "apiCall": false,
-            "links": [
-                "districtCommisioner"
-            ]
-        },
-        "districtCommisioner": {
-            "displayName": "District Commisioner",
-            "apiCall": false,
-            "links": null
-        },
-        "deptOfAgrarianDevelopment": {
-            "displayName": "Department Of Agrarian Development",
-            "apiCall": false,
-            "links": null
-        },
-        "mahaweliAuthority": {
-            "displayName": "Mahaweli Authority",
-            "apiCall": false,
-            "links": null
-        },
-        "directorDOA": {
-            "displayName": "Director DOA",
-            "apiCall": false,
-            "links": null
-        },
-        "provincialDirectorOfAgri": {
-            "displayName": "Provincial Director Of Agriculture",
-            "apiCall": false,
-            "links": null
-        },
-        "agroEcologicalZones": {
-            "displayName": "Agro Ecological Zones",
-            "apiCall": false,
-            "links": null
-        }
-    }
+
   useUserAccessValidation();
   const { state } = useLocation();
   const location = useLocation();
@@ -312,7 +181,7 @@ const UsersForm = () => {
         setSelectRoles([]);
     };
     console.log(selectRoles)
-
+    console.log(data)
     const toggleServicesSelect = (component) => {
         setSelectServices((current = []) => {
             let newList = [...current];
