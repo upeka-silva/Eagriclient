301c49d27d627eb251eefb90539725ef603a7b52
Mr RAAN <hiafraan@gmail.com>
2023-08-20 14:42:10 +0530

    Refactor form layout using Material-UI components for improved readability and cleanliness.

--- src/components/FormLayout/FieldWrapper.js
+++ src/components/FormLayout/FieldWrapper.js
@@ -1,13 +1,12 @@
+import Stack from "@mui/material/Stack";
 import styled from "styled-components";
 
-export const FieldWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
-  /* max-width: 500px; */
-  margin-bottom: 20px;
-  gap: 0px;
-  padding-inline:15px;
-  
+export const FieldWrapper = styled(Stack)`
+  && {
+    align-items: flex-start;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    gap: 0px;
+    padding-inline: 15px;
+  }
 `;
--- src/components/FormLayout/ResetButton.js
+++ src/components/FormLayout/ResetButton.js
@@ -1,14 +1,22 @@
+import Button from "@mui/material/Button";
 import styled from "styled-components";
 import { Colors } from "../../utils/constants/Colors";
 
-export const ResetButton = styled.button`
-  width: 100px;
-  height: 28px;
-  color: ${Colors.formFieldName};
-  background-color: ${Colors.white};
-  font-size: 12px;
-  font-weight: 400;
-  border: 1px solid #434343;
-  border-radius: 22px;
-  cursor: pointer;
+export const ResetButton = styled(Button)`
+  && {
+    width: 100px;
+    height: 28px;
+    border: 1px solid #434343;
+    border-radius: 22px;
+    background-color: ${Colors.white};
+    color: ${Colors.formFieldName};
+    font-size: 12px;
+    line-height: 0px;
+    box-shadow: none;
+    cursor: pointer;
+    &:hover {
+      background-color: ${Colors.white};
+      border: 1px solid #434343;
+    }
+  }
 `;
