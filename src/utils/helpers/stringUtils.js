f5eba720d75a674f97df1728817f3e71e0a7f20c
shashika <hsksilva37542@gmai.com>
2023-09-17 10:35:15 +0530

    initial changes

--- src/utils/helpers/stringUtils.js
+++ src/utils/helpers/stringUtils.js
@@ -2,13 +2,11 @@ import {getLSItem} from "../../services/storage";
 import {StorageConstants} from "../../services/storage/constant";
 
 export const isEmpty = (value) => {
-    console.log('isEmpty ', value);
     if (value === undefined || value == null) {
         return true;
     }
 
     if (value.length === 0) {
-        console.log('jjjj');
         return true;
     }
 
