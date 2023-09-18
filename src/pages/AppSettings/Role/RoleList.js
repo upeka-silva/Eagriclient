97dd9e787124542c8bd60043b3f2119e781c4dfa
chandimalraj <chandimalrajapaksha97@gmail.com>
2023-09-18 10:59:34 +0530

    update data fetch issue

--- src/pages/AppSettings/Role/RoleList.js
+++ src/pages/AppSettings/Role/RoleList.js
@@ -2,13 +2,13 @@ import React, { useEffect } from "react";
 import { TableWrapper } from "../../../components/PageLayout/TableWrapper";
 import { DataTable } from "../../../components/PageLayout/Table";
 
-const RoleList = (
+const RoleList = ({
   dataEndPoint,
   selectedRows = [],
   onRowSelect = (_c) => {},
   selectAll = (_list = []) => {},
-  unSelectAll = () => {}
-) => {
+  unSelectAll = () => {},
+}) => {
   const columns = [
     { field: "code", headerName: "Code" },
     { field: "name", headerName: "Description" },
