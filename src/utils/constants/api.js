efb55a67b0fc52c033fe96f564411cf6a37dcc63
vjayathilaka <vijithakn@gmail.com>
2024-01-12 17:38:10 +0530

    prod config changes

--- src/utils/constants/api.js
+++ src/utils/constants/api.js
@@ -1,2 +1,3 @@
-export const baseURL = "http://43.205.74.219:9090/api/v1/";
+export const baseURL = "https://43.205.75.154:9090/api/v1/";
 //export const baseURL = "http://localhost:8080/api/v1/";
+ 
