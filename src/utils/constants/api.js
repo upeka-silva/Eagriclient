1165009fc74209e348e7a2ac7d710317a8a51056
vjayathilaka <vijithakn@gmail.com>
2024-01-12 18:00:12 +0530

    miner issue

--- src/utils/constants/api.js
+++ src/utils/constants/api.js
@@ -1,3 +1,3 @@
-export const baseURL = "https://43.205.75.154:9090/api/v1/";
+export const baseURL = "http://43.205.75.154:9090/api/v1/";
 //export const baseURL = "http://localhost:8080/api/v1/";
  
