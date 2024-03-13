f237b7cba8e75b9805d6eadb4f8f5996b02ebc6b
Uditha Thilakarathana <157568367+udithathilakarathna@users.noreply.github.com>
2024-03-13 18:12:57 +0530

    Image Upload Agriculture Post

--- src/pages/Extention/CreatePostForm.js
+++ src/pages/Extention/CreatePostForm.js
@@ -1,8 +1,11 @@
 import {
+  Box,
+    CircularProgress,
     Grid,
+    IconButton,
     TextField
   } from "@mui/material";
-  import React, { useState } from "react";
+  import React, { useEffect, useState } from "react";
   import { useLocation, useNavigate } from "react-router";
   import { useSnackBars } from "../../context/SnackBarContext";
   import { useUserAccessValidation } from "../../hooks/authentication";
@@ -15,6 +18,8 @@ import {
   import { FieldWrapper } from "../../components/FormLayout/FieldWrapper";
   import { FormWrapper } from "../../../src/components/FormLayout/FormWrapper";
   import PageHeader from "../../components/PageHeader/PageHeader";
+  import { PhotoCamera } from "@mui/icons-material";
+  import { handleAgriculturePostImage } from "../../redux/actions/extension/action";
   
   
   const CreatePostForm = () => {
@@ -25,6 +30,7 @@ import {
     const navigate = useNavigate();
   
     const [formData, setFormData] = useState(state?.target || {});
+ 
     const [saving, setSaving] = useState(false);
   
     const { addSnackBar } = useSnackBars();
@@ -32,7 +38,22 @@ import {
     const goBack = () => {
       navigate("/extension/create-Post");
     };
-  
+
+    const [imageUploading, setImageUploading] = useState(false);
+
+    const [imageData, setImageData] = useState([]);
+    const [selectedFile, setSelectedFile] = useState();
+    const [selectedImage, setSelectedImage] = useState(
+      state?.target?.presignedUrl || null
+    );
+    const [selectedImage1, setSelectedImage1] = useState(
+      state?.target?.presignedUrl || null
+    );
+    const [selectedImage2, setSelectedImage2] = useState(
+      state?.target?.presignedUrl || null
+    );
+    const [form, setForm] = useState();
+
     const handleChange = (value, target) => {
       setFormData((current = {}) => {
         let newData = { ...current };
@@ -48,6 +69,7 @@ import {
         setFormData({});
       }
     };
+    // const [selectedFile, setSelectedFile] = useState(null);
   
     const enableSave = () => {
       if (state?.action === DEF_ACTIONS.EDIT) {
@@ -82,27 +104,114 @@ import {
       });
       setSaving(false);
     };
-  
+
     const handleFormSubmit = async () => {
-      if (enableSave()) {
+      if (true) {
         setSaving(true);
+  
         try {
-          if (formData?.id) {
-            await updateAgriculturePost(formData, onSuccess, onError);
-          } else {
-            await handleAgriculturePost(formData, onSuccess, onError);
+          if (true) {
+            console.log("forms",formData)
+
+            const postForm = {...formData, images: imageData};
+
+            const response = await handleAgriculturePost(
+              postForm,
+              onSuccess,
+              onError
+            );
+
+            // if ((res.httpCode = "200 OK")) {
+            //   const postImageUrl = res.payload.storedFileName;
+            //   const originalFileName = res.payload.originalFileName;
+            //   const prsignedUrl = res.payload.presignedUrl;
+            //   const presignedExpDate = res.payload.expireDate;
+            //   console.log("URL",postImageUrl)
+  
+            //   await updateAgriculturePost(
+            //     {
+            //       ...formData,
+            //       id: response.payload?.id,
+            //       postImageUrl: postImageUrl,
+            //       originalFileName: originalFileName,
+            //       presignedUrl: prsignedUrl,
+            //       presignedExpDate: presignedExpDate,
+            //     },
+            //     onSuccess,
+            //     onError
+            //   );
+            // }
+            return;
           }
+          setSaving(false);
         } catch (error) {
           console.log(error);
         }
       }
     };
   
-    const getPathName = () => {
-      return location.pathname === "/" || !location.pathname
-        ? ""
-        : location.pathname;
+    const handleImageChange = async (event) => {
+      const file = event.target.files[0];
+      const file2 = event.target.name;
+      setSelectedFile(file);
+      console.log("img fun", file);
+    
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          console.log(reader.result);
+          setSelectedImage(reader.result);
+          
+          // Logging FormData payload after it's fully populated
+          const form = new FormData();
+          form.append("file", file);
+   
+          setImageUploading(true);
+          handleAgriculturePostImageUpload(form).then(data => {
+            console.log("image response --------->");
+            console.log(data.payload.storedFileName);
+            setImageData([...imageData, {
+              postImageUrl: data.payload.storedFileName,
+              originalFileName: data.payload.originalFileName,
+              presignedUrl: data.payload.presignedUrl,
+              presignedExpDate: data.payload.expireDate,
+            }]);
+            setImageUploading(false);
+          });
+
+          
+        };
+        reader.readAsDataURL(file);
+      } else {
+        setSelectedImage(null);
+        setForm(null);
+      }
     };
+    
+  
+  
+    const handleAgriculturePostImageUpload = async (file) => {
+      try {
+        const response = await handleAgriculturePostImage(file,
+          () => onSuccess("Success"), // Pass onSuccess as a callback function
+          onError
+        );
+    
+        return response;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+  
+  
+  
+  
+    // const getPathName = () => {
+    //   return location.pathname === "/" || !location.pathname
+    //     ? ""
+    //     : location.pathname;
+    // };
   
     return (
       <FormWrapper>
@@ -245,6 +354,220 @@ import {
               />
             </FieldWrapper>
           </Grid>
+          <Grid item lg={4}>
+            <Grid container>
+              <Grid item sm={3} md={3} lg={9}>
+                {!imageUploading ? <FieldWrapper>
+                  <FieldName>Select Image 01</FieldName>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    <input
+                      type="file"
+                      name=""
+                      accept="image/*"
+                      id="profile-picture-input"
+                      style={{ display: "none" }}
+                      onChange={handleImageChange}
+                    />
+
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                      sx={{ position: "relative" }}
+                    >
+                      <label
+                        htmlFor="profile-picture-input"
+                        style={{
+                          width: "320px",
+                          height: "250px",
+                          border: "1px solid #7a879d",
+                          borderRadius: "8px",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          backgroundColor: "rgb(46,125,50,0.1)",
+                        }}
+                      >
+                        <IconButton component="span" style={{ zIndex: "2" }}>
+                          <PhotoCamera />
+                        </IconButton>
+                      </label>
+                      {selectedImage && (
+                        <div
+                          style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            backgroundColor: "rgb(46,125,50,0.1)",
+                            width: "320px",
+                            height: "250px",
+                            borderRadius: "8px",
+                          }}
+                        >
+                          <img
+                            src={selectedImage}
+                            alt="Profile"
+                            style={{
+                              width: "320px",
+                              height: "250px",
+                              borderRadius: "8px",
+                            }}
+                          />
+                        </div>
+                      )}
+                    </Box>
+                  </div>
+                </FieldWrapper> : <CircularProgress />}
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* <Grid item lg={4}>
+            <Grid container>
+              <Grid item sm={3} md={3} lg={9}>
+                <FieldWrapper>
+                  <FieldName>Select Image 02</FieldName>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    <input
+                      type="file"
+                      accept="image/*"
+                      id="profile-picture-input"
+                      style={{ display: "none" }}
+                      onChange={handleImageChange}
+                    />
+
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                      sx={{ position: "relative" }}
+                    >
+                      <label
+                        htmlFor="profile-picture-input"
+                        style={{
+                          width: "320px",
+                          height: "250px",
+                          border: "1px solid #7a879d",
+                          borderRadius: "8px",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          backgroundColor: "rgb(46,125,50,0.1)",
+                        }}
+                      >
+                        <IconButton component="span" style={{ zIndex: "2" }}>
+                          <PhotoCamera />
+                        </IconButton>
+                      </label>
+                      {selectedImage && (
+                        <div
+                          style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            backgroundColor: "rgb(46,125,50,0.1)",
+                            width: "320p",
+                            height: "250px",
+                            borderRadius: "8px",
+                          }}
+                        >
+                          <img
+                            src={selectedImage}
+                            alt="Profile"
+                            style={{
+                              width: "320p",
+                              height: "250px",
+                              borderRadius: "8px",
+                            }}
+                          />
+                        </div>
+                      )}
+                    </Box>
+                  </div>
+                </FieldWrapper>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item lg={4}>
+            <Grid container>
+              <Grid item sm={3} md={3} lg={9}>
+                <FieldWrapper>
+                  <FieldName>Select Image 03</FieldName>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                  >
+                    <input
+                      type="file"
+                      accept="image/*"
+                      id="profile-picture-input"
+                      style={{ display: "none" }}
+                      onChange={handleImageChange}
+                    />
+
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      alignItems="center"
+                      sx={{ position: "relative" }}
+                    >
+                      <label
+                        htmlFor="profile-picture-input"
+                        style={{
+                          width: "320px",
+                          height: "250px",
+                          border: "1px solid #7a879d",
+                          borderRadius: "8px",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          backgroundColor: "rgb(46,125,50,0.1)",
+                        }}
+                      >
+                        <IconButton component="span" style={{ zIndex: "2" }}>
+                          <PhotoCamera />
+                        </IconButton>
+                      </label>
+                      {selectedImage && (
+                        <div
+                          style={{
+                            position: "absolute",
+                            zIndex: "1",
+                            backgroundColor: "rgb(46,125,50,0.1)",
+                            width: "320px",
+                            height: "250px",
+                            borderRadius: "8px",
+                          }}
+                        >
+                          <img
+                            src={selectedImage}
+                            alt="Profile"
+                            style={{
+                              width: "320px",
+                              height: "250px",
+                              borderRadius: "8px",
+                            }}
+                          />
+                        </div>
+                      )}
+                    </Box>
+                  </div>
+                </FieldWrapper>
+              </Grid>
+            </Grid>
+          </Grid> */}
         </Grid>
       </FormWrapper>
     );
