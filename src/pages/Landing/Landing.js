c4cf13232a16d62cbd78ad5338e821754e72f990
hiruna gayashan <hirunagrad@gmail.com>
2024-03-19 09:06:46 +0530

    Add Cropix image and update landing page layout

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -7,6 +7,7 @@ import {
 } from "@mui/material";
 import React from "react";
 import MainLogo from "../../assets/images/DepartmentOfAgricultureLogo.png";
+import Cropix from "../../assets/images/Cropix.png";
 import SearchIcon from "@mui/icons-material/Search";
 import Samba from "../../assets/images/sambasample.png";
 import LandingCarousal from "../../assets/images/landingcarasol.png";
@@ -21,8 +22,57 @@ function Landing() {
     <div>
       <Grid container px={5} mt={3} sx={{ width: "100vw" }}>
         <Grid
+          md={12}
+          item
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <Grid sx={{ display: "flex", justifyContent: "flex-start" }}>
+            <img width={"35%"} src={Cropix} alt="cropix" />
+            <Typography ml={5} fontWeight={'bold'} color={'#666666'} >
+              Crop Resources, Optimizing Operations <br/> through Precise Information,
+              Exchange System
+            </Typography>
+          </Grid>
+
+          <Grid>
+        <Button
+            sx={{ 
+                fontSize: "12px", 
+                color: "#ffff", 
+                backgroundColor: "#158FD0", 
+                borderRadius: "25px", // adjust this value as needed
+                ':hover': {
+                    backgroundColor: "#158FD0", // same as normal state
+                    color: "#ffff" // same as normal state
+                }
+            }}
+            href="/login"
+            color="primary"
+        >
+            Login
+        </Button>
+          {/* <Button
+            sx={{ 
+              fontSize: "12px", 
+              color: "#ffff", 
+              backgroundColor: "#158FD0", 
+              borderRadius: "15px", // adjust this value as needed
+              marginLeft: "10px" // adjust this value as needed
+            }}
+            href="/signup"
+            color="primary"
+          >
+            Sign Up
+          </Button> */}
+          </Grid>
+        </Grid>
+        <Grid md={12} mt={1}>
+          <hr />
+        </Grid>
+        <Grid
           item
           md={12}
+          mt={3}
           sx={{
             display: "flex",
             justifyContent: "space-between",
@@ -31,7 +81,7 @@ function Landing() {
           <Grid>
             <img width={"30%"} src={MainLogo} alt="Logo" />
           </Grid>
-          <Typography fontSize={"25px"}>
+          {/* <Typography fontSize={"25px"}>
             <Button
               sx={{ fontSize: "15px", color: "#000" }}
               href="/login"
@@ -46,7 +96,7 @@ function Landing() {
             >
               Sign Up
             </Button>
-          </Typography>
+          </Typography> */}
         </Grid>
         <Grid container display={"flex"} sx={{ flexDirection: "row" }}>
           <Grid item>
@@ -114,28 +164,27 @@ function Landing() {
           <Grid item md={7}>
             <img src={LandingCarousal} alt="Samba" width={"100%"} />
             <Grid
-            
-            mt={17}
-            style={{ display: "flex", flexWrap: "wrap", height: "10%" }}
-          >
-            <CustomCard
-              imageUrl={CultivateImg}
-              title="Paddy Cultivation Extent"
-              extent={"750000 ha"}
-            />
-            <CustomCard
-              imageUrl={CultivateImg}
-              title="MAIZE Cultivation Extent"
-              extent={"100,000 ha"}
-            />
-            <CustomCard
-              imageUrl={CultivateImg}
-              title="Paddy Cultivation Extent"
-              extent={"120 mt"}
-            />
+              mt={17}
+              style={{ display: "flex", flexWrap: "wrap", height: "10%" }}
+            >
+              <CustomCard
+                imageUrl={CultivateImg}
+                title="Paddy Cultivation Extent"
+                extent={"750000 ha"}
+              />
+              <CustomCard
+                imageUrl={CultivateImg}
+                title="MAIZE Cultivation Extent"
+                extent={"100,000 ha"}
+              />
+              <CustomCard
+                imageUrl={CultivateImg}
+                title="Paddy Cultivation Extent"
+                extent={"120 mt"}
+              />
+            </Grid>
           </Grid>
-          </Grid>
-          
+
           <Grid item md={3}>
             <Grid sx={{ width: "70%", paddingLeft: "190px" }}>
               <FaoEmergencyMap />
