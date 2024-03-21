49ec32eba5623aa806d8d6f9260208055d5a329a
vjayathilaka <vijithakn@gmail.com>
2024-03-21 19:15:07 +0530

    change props for stage

--- src/utils/constants/api.js
+++ src/utils/constants/api.js
@@ -1,3 +1,3 @@
-export const baseURL = "https://digital.doa.gov.lk:9090/api/v1/";
+//export const baseURL = "https://digital.doa.gov.lk:9090/api/v1/";
 // export const baseURL = "http://localhost:8080/api/v1/";
-//export const baseURL = "http://43.205.74.219:9090/api/v1/";
+export const baseURL = "http://43.205.74.219:9090/api/v1/";
