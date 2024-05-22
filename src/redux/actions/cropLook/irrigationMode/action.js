6f3d4b983d23e59e3784bf162e5742315605df3b
hiruna gayashan <hirunagrad@gmail.com>
2024-05-22 13:06:38 +0530

    chore: Refactor irrigation mode progress and variety progress actions

--- src/redux/actions/cropLook/irrigationMode/action.js
+++ src/redux/actions/cropLook/irrigationMode/action.js
@@ -1,67 +1,91 @@
 import { get } from "../../../../services/api";
 
-export const getIrrigationModeProgress = async (seasonId, categoryId,cropId) => {
-    try {
-      const { httpCode, payloadDto } = await get(`crop-look/dd-report/varietyProgress/category/${categoryId}/crop/${cropId}/season/${seasonId}`, true);
-      if (httpCode === "200 OK") {
-        return {
-          dataList: payloadDto,
-        };
-      }
+export const getIrrigationModeProgress = async (
+  seasonId,
+  categoryId,
+  cropId
+) => {
+  try {
+    const { httpCode, payloadDto } = await get(
+      `crop-look/dd-report/varietyProgress/category/${categoryId}/crop/${cropId}/season/${seasonId}`,
+      true
+    );
+    if (httpCode === "200 OK") {
       return {
-        dataList: [],
-      };
-    } catch (error) {
-      console.log(error);
-      return {
-        dataList: [],
+        dataList: payloadDto,
       };
     }
-  };
+    return {
+      dataList: [],
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      dataList: [],
+    };
+  }
+};
 
-
-  export const getvarietyProgress = async (seasonId, categoryId,cropId) => {
-    
-    try {
-      const { httpCode, payloadDto } = await get(`crop-look/dd-report/irrigationModeProgress/category/${categoryId}/crop/${cropId}/season/${seasonId}`, true);
-      console.log({payloadDto})
-      if (httpCode === "200 OK") {
-        return {
-          dataList: payloadDto,
-        };
-      }
+export const getvarietyProgress = async (seasonId, categoryId, cropId) => {
+  try {
+    const { httpCode, payloadDto } = await get(
+      `crop-look/dd-report/irrigationModeProgress/category/${categoryId}/crop/${cropId}/season/${seasonId}`,
+      true
+    );
+    console.log({ payloadDto });
+    if (httpCode === "200 OK") {
       return {
-        dataList: [],
-      };
-    } catch (error) {
-      console.log(error);
-      return {
-        dataList: [],
-      };
-    }
-  };
-
-
-  export const getTargetExtent = async (seasonId, categoryId) => {
-    
-    try {
-      const { httpCode, payloadDto } = await get(`crop-look/dd-report/targetExtentByDistrict/category/${categoryId}/season/${seasonId}`, true);
-      console.log({payloadDto})
-      if (httpCode === "200 OK") {
-        return {
-          dataList: payloadDto,
-        };
-      }
-      return {
-        dataList: [],
-      };
-    } catch (error) {
-      console.log(error);
-      return {
-        dataList: [],
+        dataList: payloadDto,
       };
     }
-  };
+    return {
+      dataList: [],
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      dataList: [],
+    };
+  }
+};
 
+export const getTargetExtent = async (seasonId, categoryId) => {
+  try {
+    const { httpCode, payloadDto } = await get(
+      `crop-look/dd-report/targetExtentByDistrict/category/${categoryId}/season/${seasonId}`,
+      true
+    );
+    console.log({ payloadDto });
+    if (httpCode === "200 OK") {
+      return {
+        dataList: payloadDto,
+      };
+    }
+    return {
+      dataList: [],
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      dataList: [],
+    };
+  }
+};
 
-  
+export const getProgressBiweekly = async (seasonId, categoryId, cropId) => {
+  try {
+    const payloadDto = await get(
+      `crop-look/getProgressBiweekly/cropCategory/${categoryId}/crop/${cropId}/season/${seasonId}`,
+      true
+    );
+
+    return {
+      dataList: payloadDto,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      dataList: [],
+    };
+  }
+};
